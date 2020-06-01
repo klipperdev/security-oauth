3b177da4525e280296932d64bf0e56d96e77b08e
François Pluchino <francois.pluchino@klipper.dev>
2020-06-01 20:04:34 +0200

    Allow to use the OauthScope class with auth checker

--- Authorization/Voter/OauthScopeVoter.php
+++ Authorization/Voter/OauthScopeVoter.php
@@ -12,6 +12,8 @@
 namespace Klipper\Component\SecurityOauth\Authorization\Voter;
 
 use Klipper\Component\Security\Authorization\Voter\AbstractIdentityVoter;
+use Klipper\Component\SecurityOauth\Annotation\OauthScope;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 /**
  * @author François Pluchino <francois.pluchino@klipper.dev>
@@ -23,11 +25,60 @@ class OauthScopeVoter extends AbstractIdentityVoter
         return 'oauth_scope';
     }
 
-    /**
-     * {@inheritdoc}
-     */
     protected function getDefaultPrefix(): string
     {
         return 'scope:';
+    }
+
+    protected function supports($attribute, $subject): bool
+    {
+        return parent::supports($attribute, $subject) || $attribute instanceof OauthScope;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token): bool
+    {
+        if (!$attribute instanceof OauthScope) {
+            return parent::voteOnAttribute($attribute, $subject, $token);
+        }
+
+        $tokenScopes = $this->getTokenScopes($token);
+
+        if ($attribute->isAllRequired()) {
+            foreach ($attribute->getScope() as $scope) {
+                if (!\in_array($scope, $tokenScopes, true)) {
+                    return false;
+                }
+            }
+        } else {
+            $access = false;
+
+            foreach ($attribute->getScope() as $scope) {
+                if (\in_array($scope, $tokenScopes, true)) {
+                    $access = true;
+
+                    break;
+                }
+            }
+
+            if (!$access) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function getTokenScopes(TokenInterface $token): array
+    {
+        $sids = $this->sim->getSecurityIdentities($token);
+        $scopes = [];
+
+        foreach ($sids as $sid) {
+            if ($sid->getType() === $this->getValidType()) {
+                $scopes[] = $sid->getIdentifier();
+            }
+        }
+
+        return $scopes;
     }
 }
